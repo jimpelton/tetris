ed5dbc1ae8aac4e5d4f78091e5c4c2bdde71018e
Jim Pelton <jimpelton@blacksagetech.com>
2021-08-01 16:22:18 -0600

    falling blocks, wrong size though

--- tetrimino.py
+++ tetrimino.py
@@ -1,52 +1,122 @@
 import pygame as pg
 
+colors = [
+    (0, 0, 0),
+    (255, 85, 85),
+    (100, 200, 115),
+    (120, 108, 245),
+    (255, 140, 50),
+    (50, 120, 52),
+    (146, 202, 73),
+    (150, 161, 218),
+    (35, 35, 35),
+]
+
 
 class Block(pg.sprite.Sprite):
-    pass
+    def __init__(self, board_col, board_row, width, color) -> None:
+        super().__init__()
+        self.board_pos = [board_col, board_row]
+        self.width = width
+        self.color = color
+        # use the board-relative x, y (col, row)
+        self.rect = pg.Rect(board_col * width, board_row * width, width, width)
+        self.image = pg.Surface([width, width])
+        self.image.fill(color)
+
+    def get_board_pos(self):
+        return self.board_pos
+
+    def update(self, cols, rows):
+        self.board_pos[0] += cols
+        self.board_pos[1] += rows
+        self.rect = self.rect.move(cols * self.width, rows * self.width)
+        # self.rect = pg.Rect(
+        # board_col * self.width,
+        # self.board_pos[1] + board_row * self.width,
+        # self.width,
+        # self.width,
+        # )
+
+    # def draw(self, surface):
+    # pg.draw.rect(surface, self.color, self.rect)
 
 
 class Tetrimino:
     BOARD = []
-    def __init__(self) -> None:
-        pass
+    BLOCK_PX_WIDTH = 10
+    COLOR = colors[1]
+
+    def __init__(self, *args, **kwargs) -> None:
+        self.group = pg.sprite.Group()
+        self.board_args = kwargs
+
+        self.create_blocks(**kwargs)
+
+    def create_blocks(self, **kwargs):
+        board_rect = kwargs["rect"]
+        board_cols = kwargs["cols"]
+        board_rows = kwargs["rows"]
+        block_px = int(board_rect.height / board_rows) # TODO: this isn't right
+        for r_i, row in enumerate(self.BOARD):
+            for c_i, val in enumerate(row):
+                if val:
+                    b = Block(c_i, r_i, block_px, self.COLOR)
+                    self.group.add(b)
+
+    def can_move_by(self, cols, rows):
+        board_rows = self.board_args["rows"]
+        board_cols = self.board_args["cols"]
+        board_sprites = self.board_args["cell_sprites"]
+
+        def hits(b):
+            pos = b.get_board_pos()
+            new_col = pos[0] + cols
+            new_row = pos[1] + rows
+            print(f"New Col: {new_col}, New Row: {new_row}")
+
+            try:
+                return bool(board_sprites[new_row][new_col])
+            except IndexError:
+                # we hit the wall of the board
+                return True
+
+        return bool([b for b in self.group.sprites() if not hits(b)])
+
+    def update(self, cols, rows):
+        self.group.update(cols, rows)
+
+    def draw(self, surface):
+        self.group.draw(surface)
 
 
 class I(Tetrimino):
     BOARD = [
-        [0, 0, 1, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 0],
+        [1, 1, 1, 1],
     ]
+    COLOR = colors[1]
 
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
 
 class J(Tetrimino):
     BOARD = [
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
+        [1, 0, 0],
+        [1, 1, 1],
     ]
+    COLOR = colors[2]
 
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
 
 class L(Tetrimino):
     BOARD = [
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
+        [0, 0, 1],
+        [1, 1, 1],
     ]
+    COLOR = colors[3]
 
     def __init__(self) -> None:
         super().__init__()
@@ -54,13 +124,10 @@ class L(Tetrimino):
 
 class O(Tetrimino):
     BOARD = [
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
+        [1, 1],
+        [1, 1],
     ]
+    COLOR = colors[4]
 
     def __init__(self) -> None:
         super().__init__()
@@ -68,13 +135,10 @@ class O(Tetrimino):
 
 class S(Tetrimino):
     BOARD = [
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
+        [0, 1, 1],
+        [1, 1, 0],
     ]
+    COLOR = colors[5]
 
     def __init__(self) -> None:
         super().__init__()
@@ -82,13 +146,10 @@ class S(Tetrimino):
 
 class T(Tetrimino):
     BOARD = [
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
+        [0, 1, 0],
+        [1, 1, 1],
     ]
+    COLOR = colors[6]
 
     def __init__(self) -> None:
         super().__init__()
@@ -96,13 +157,10 @@ class T(Tetrimino):
 
 class Z(Tetrimino):
     BOARD = [
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
+        [1, 1, 0],
+        [0, 1, 1],
     ]
+    COLOR = colors[7]
 
     def __init__(self) -> None:
         super().__init__()
--- tetris.py
+++ tetris.py
@@ -1,9 +1,67 @@
 import pygame as pg
 from pygame.locals import *
 
+import tetrimino
+
 pg.init()
 if not pg.get_init():
     print("oh no! pygame not init'd")
     exit(1)
 
+SCREENRECT = pg.Rect(0, 0, 720, 1280)
+BOARD_COLS = 10
+BOARD_ROWS = 20
+BOARD = {
+    "cells": [[0] * BOARD_COLS] * BOARD_ROWS,
+    "cols": BOARD_COLS,
+    "rows": BOARD_ROWS,
+    "rect": pg.Rect(10, 10, SCREENRECT.width * 0.75, SCREENRECT.height * 0.95),
+    "cell_sprites": [[None] * BOARD_COLS] * BOARD_ROWS,
+    "color": (0, 0, 0),
+}
 
+
+class BoardSprite(pg.sprite.Sprite):
+    def __init__(self, **board) -> None:
+        super().__init__()
+        self.board = board
+
+        self.image = pg.Surface((board["rect"].width, board["rect"].height))
+        self.image.fill(board["color"])
+        self.rect = self.image.get_rect()
+
+        pg.draw.rect(self.image, (255, 255, 255), self.rect, width=1)
+
+
+def main():
+    screen = pg.display.set_mode(SCREENRECT.size)
+    clock = pg.time.Clock()
+    #    sprites = pg.sprite.Group()
+    # row = 0
+    # col = 0
+    # block = tetrimino.Block(col, row, 10, tetrimino.colors[1])
+    group = pg.sprite.Group()
+    tet = tetrimino.J(**BOARD)
+    board_sprite = BoardSprite(**BOARD)
+    group.add(board_sprite)
+
+    while True:
+        clock.tick(1)
+        if tet.can_move_by(0, 1):
+            tet.update(0, 1)
+        else:
+            print("tet can't move there")
+
+        screen.fill(tetrimino.colors[0])
+        group.draw(screen)
+        tet.draw(screen)
+
+        pg.display.flip()
+
+
+def rotate(tetrimino):
+    pass
+
+
+if __name__ == "__main__":
+    main()
